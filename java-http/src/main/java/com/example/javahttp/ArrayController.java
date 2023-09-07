1899ad382545da482d744490a4475548530fbfe0
luizdopc <luizotaviosoares@outlook.com>
2023-09-07 12:58:52 -0300

    first metric working

--- java-http/src/main/java/com/example/javahttp/ArrayController.java
+++ java-http/src/main/java/com/example/javahttp/ArrayController.java
@@ -1,16 +1,30 @@
 package com.example.javahttp;
 
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ArrayController {
+
+    private final Counter requestCounter;
+
+    public ArrayController(MeterRegistry registry){
+        this.requestCounter = Counter
+                .builder("request_counter")
+                .description("Contador de requisições")
+                .register(registry);
+    }
+
     @PostMapping("/foo")
     public int helloWorld(@RequestBody int[] numbers) {
         for(int i=0; i<numbers.length; i++){
             System.out.println(i);
         }
+
+        requestCounter.increment();
         return -1;
     }
 }
