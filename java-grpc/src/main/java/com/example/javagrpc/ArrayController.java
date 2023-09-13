102d35b07182f0431b7b3c227cdf92f97f5facc5
luizdopc <luizotaviosoares@outlook.com>
2023-09-13 20:40:11 -0300

    java grpc working

--- java-grpc/src/main/java/com/example/javagrpc/ArrayController.java
+++ java-grpc/src/main/java/com/example/javagrpc/ArrayController.java
@@ -1,5 +1,7 @@
 package com.example.javagrpc;
 
+import io.micrometer.core.instrument.Metrics;
+
 public class ArrayController extends ArrayServiceGrpc.ArrayServiceImplBase {
 
     @Override
@@ -9,8 +11,10 @@ public class ArrayController extends ArrayServiceGrpc.ArrayServiceImplBase {
         int[] numbers = request.getArrayList().stream().mapToInt(Integer::intValue).toArray();
 
         for(int i=0; i< numbers.length; i++){
-            System.out.println(numbers[i]);
+            // System.out.println(numbers[i]);
         }
+
+        Metrics.counter("request_counter").increment();
 
         responseObserver.onNext(com.example.javagrpc.ArrayDefinition.Num.newBuilder().setNum(-1).build());
         responseObserver.onCompleted();
--- java-grpc/src/main/java/com/example/javagrpc/JavaGrpcApplication.java
+++ java-grpc/src/main/java/com/example/javagrpc/JavaGrpcApplication.java
@@ -1,15 +1,7 @@
 package com.example.javagrpc;
 
-import io.grpc.BindableService;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
-import io.grpc.ServerInterceptors;
-import io.micrometer.core.instrument.Counter;
-import io.micrometer.core.instrument.MeterRegistry;
-import io.micrometer.core.instrument.cumulative.CumulativeCounter;
-import io.micrometer.prometheus.PrometheusConfig;
-import io.micrometer.prometheus.PrometheusMeterRegistry;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -23,10 +15,8 @@ public class JavaGrpcApplication {
 
         int port = 50051;
 
-        MeterRegistry registry = new PrometheusMeterRegistry(PrometheusConfig.DEFAULT);
-
         Server server = ServerBuilder.forPort(port)
-                .addService(ServerInterceptors.intercept((BindableService) new ArrayController(), new GrpcServerRequestInterceptor(registry)))
+                .addService(new ArrayController())
                 .build();
 
         System.out.println("Starting gRPC server on port " + port);
