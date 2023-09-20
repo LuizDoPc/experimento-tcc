29e40b62a7e4af19f8c6c070aeee85073522ca64
luizdopc <luizotaviosoares@outlook.com>
2023-09-20 01:00:28 -0300

    timer metric in java

--- java-grpc/src/main/java/com/example/javagrpc/ArrayController.java
+++ java-grpc/src/main/java/com/example/javagrpc/ArrayController.java
@@ -2,19 +2,25 @@ package com.example.javagrpc;
 
 import io.micrometer.core.instrument.Metrics;
 
+import java.util.concurrent.TimeUnit;
+
 public class ArrayController extends ArrayServiceGrpc.ArrayServiceImplBase {
 
     @Override
     public void search(com.example.javagrpc.ArrayDefinition.Array request,
                        io.grpc.stub.StreamObserver<com.example.javagrpc.ArrayDefinition.Num> responseObserver){
-
+        long start = System.nanoTime();
         int[] numbers = request.getArrayList().stream().mapToInt(Integer::intValue).toArray();
 
         for(int i=0; i< numbers.length; i++){
-            // System.out.println(numbers[i]);
+             System.out.println(numbers[i]);
         }
 
         Metrics.counter("request_counter").increment();
+        Metrics.timer(
+                "request_timer",
+                "c", Double.toString(Metrics.counter("request_counter").count())
+        ).record(System.nanoTime() - start, TimeUnit.NANOSECONDS);
 
         responseObserver.onNext(com.example.javagrpc.ArrayDefinition.Num.newBuilder().setNum(-1).build());
         responseObserver.onCompleted();
--- java-http/src/main/java/com/example/javahttp/ArrayController.java
+++ java-http/src/main/java/com/example/javahttp/ArrayController.java
@@ -2,16 +2,22 @@ package com.example.javahttp;
 
 import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Timer;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
 
 @RestController
 public class ArrayController {
 
     private final Counter requestCounter;
+    private final MeterRegistry currentRegistry;
 
     public ArrayController(MeterRegistry registry){
+        this.currentRegistry = registry;
+
         this.requestCounter = Counter
                 .builder("request_counter")
                 .description("Contador de requisições")
@@ -20,11 +26,15 @@ public class ArrayController {
 
     @PostMapping("/foo")
     public int helloWorld(@RequestBody int[] numbers) {
+        long start = System.nanoTime();
+        Timer t = currentRegistry.timer("request_timer", "c", Double.toString(requestCounter.count()));
+
         for(int i=0; i<numbers.length; i++){
             System.out.println(i);
         }
 
         requestCounter.increment();
+        t.record(System.nanoTime() - start, TimeUnit.NANOSECONDS);
         return -1;
     }
 }
