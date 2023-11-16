4d0bc53b5529db780d2ef9d311158ce1d391eaaf
luizdopc <luizotaviosoares@outlook.com>
2023-11-16 18:06:43 -0300

    Updated data files and code for boxplot visualization

--- boxplot.R
+++ boxplot.R
@@ -1,27 +1,27 @@
 library(tidyverse)
 library(readr)
-javagrpc_pequeno <- read_csv("dados/pequeno/javagrpc-pequeno.csv")
-javahttp_pequeno <- read_csv("dados/pequeno/javahttp-pequeno.csv")
-gohttp_pequeno <- read_csv("dados/pequeno/gohttp-pequeno.csv")
-gogrpc_pequeno <- read_csv("dados/pequeno/gogrpc-pequeno.csv")
+javagrpc_pequeno <- read_csv("dados/pequeno/v2/javagrpc-pequeno.csv")
+javahttp_pequeno <- read_csv("dados/pequeno/v2/javahttp-pequeno.csv")
+gohttp_pequeno <- read_csv("dados/pequeno/v2/gohttp-pequeno.csv")
+gogrpc_pequeno <- read_csv("dados/pequeno/v2/gogrpc-pequeno.csv")
 javahttp_grande <- read_csv("dados/grande/javahttp-grande.csv")
 javagrpc_grande <- read_csv("dados/grande/javagrpc-grande.csv")
 gohttp_grande <- read_csv("dados/grande/gohttp-grande.csv")
 gogrpc_grande <- read_csv("dados/grande/gogrpc-grande.csv")
 
-all_data <- bind_rows(
-  mutate(gogrpc_pequeno, application="gogrpc"),
-  mutate(gohttp_pequeno, application="gohttp"),
-  mutate(javahttp_pequeno, application="javahttp"),
-  mutate(javagrpc_pequeno, application="javagrpc")
-)
-
 # all_data <- bind_rows(
-#   mutate(gogrpc_grande, application="gogrpc"),
-#   mutate(gohttp_grande, application="gohttp"),
-#   mutate(javahttp_grande, application="javahttp"),
-#   mutate(javagrpc_grande, application="javagrpc")
+#   mutate(gogrpc_pequeno, application="gogrpc"),
+#   mutate(gohttp_pequeno, application="gohttp"),
+#   mutate(javahttp_pequeno, application="javahttp"),
+#   mutate(javagrpc_pequeno, application="javagrpc")
 # )
+
+all_data <- bind_rows(
+  mutate(gogrpc_grande, application="gogrpc"),
+  mutate(gohttp_grande, application="gohttp"),
+  mutate(javahttp_grande, application="javahttp"),
+  mutate(javagrpc_grande, application="javagrpc")
+)
 
 
 all_data <- all_data %>%
